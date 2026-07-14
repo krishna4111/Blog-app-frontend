efdd1d3241c72eb42ceebfe4c31501b25e9c7c2e
krishnamoorthyBFX <krishnamoorthy.k@bfxinfotech.com>
2026-07-14 21:20:36 +0530

    feat: make private routes

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import Header from "./components/Header";
 import FooterComponent from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
@@ -18,7 +19,9 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/sign-up" element={<Signup />} />
         <Route path="/sign-in" element={<Signin />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
         <Route path="/projects" element={<Projects />} />
       </Routes>
       <FooterComponent />
